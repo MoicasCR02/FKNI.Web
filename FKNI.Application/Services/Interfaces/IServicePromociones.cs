b88ab27e0e41a65545d62468bc1fd59c886a6095
dan2510 <carballo2597@gmail.com>
2025-07-29 14:46:38 -0600

    Comdificacion en el frontEnd de agregar promo

--- FKNI.Application/Services/Interfaces/IServicePromociones.cs
+++ FKNI.Application/Services/Interfaces/IServicePromociones.cs
@@ -11,5 +11,6 @@ namespace FKNI.Application.Services.Interfaces
     {
         Task<ICollection<PromocionesDTO>> ListAsync();
         Task<PromocionesDTO > FindByIdAsync(int id_promocion);
+
     }
 }
